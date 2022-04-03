e0137fb52ad1905ea780997c1aff0d0b5a3cb021
Katie Wallshein <kwallshein@ufl.edu>
2022-04-03 12:36:48 -0400

    note saves, name does not upon refresh

---
+++ index.js
@@ -0,0 +1,49 @@
+// acquire references to page elements
+var nameSpan = document.querySelector('span')
+var formEl = document.querySelector('form')
+var clear = document.querySelector('#clear')
+var textarea = document.querySelector('textarea')
+
+// Retrieve name and note content from cookies and localstorage
+// Then apply them to elements on the page
+// YOUR CODE HERE
+
+nameCookie = document.cookie;
+if (nameCookie) {
+  nameSpan.textContent = nameCookie.split('=')[1];
+}
+
+textarea.value = localStorage.getItem('textarea');
+
+
+
+formEl.onsubmit = function(e) {
+  // prevents form submission
+  e.preventDefault()
+  // save name element's content to cookies
+  // save textarea's content to localstorage
+  // YOUR CODE HERE
+  document.cookie = nameSpan.textContent;
+    localStorage.setItem('textarea',textarea.value);
+  // triggers thumbs up animation
+  this.elements.save.classList.add('emoji')
+}
+
+clear.onclick = function() {
+  // Clear textarea's value
+  // Clear localstorage's content
+  // YOUR CODE HERE
+  textarea.value = " ";
+  localStoreage.clear();
+
+  // triggers thumbs up animation
+  this.classList.add('emoji')
+}
+
+// this code allows repeated thumbs up animations
+function endThumbsUp() {
+  this.classList.remove('emoji')
+}
+
+formEl.elements.save.onanimationend = endThumbsUp
+clear.onanimationend = endThumbsUp
